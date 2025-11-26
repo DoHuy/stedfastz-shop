7b7110dde3d8e71acbb8756cabc3b5c247dec905
Huyboxi1 <huyboxi1@example.com>
2025-11-26 17:07:43 +0700

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      name: 'stedfastz-shop',
+      script: 'npm',
+      args: 'start',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      log_date_format: 'YYYY-MM-DD HH:mm Z',
+      out_file: './logs/out.log',
+      error_file: './logs/error.log',
+      max_restarts: 10,
+      min_uptime: '5s',
+      env: {
+        NODE_ENV: 'production',
+      },
+      env_file: '.env',
+    },
+  ],
+};
